7fbbd6f06e79433247e7648189dd71acc1711e52
Ethan <65051086+waitblock@users.noreply.github.com>
2020-12-31 11:02:48 -0500

    Sample Final Project

---
+++ final_project_sample.py
@@ -0,0 +1,11 @@
+number = int(input("Enter a number: "))
+steps = 0
+while number != 1:
+    print(number)
+    steps = steps + 1
+    if number % 2 == 0:
+        number = number / 2
+    else:
+        number = number * 3 + 1
+    
+print("Total Steps: " + str(steps))
